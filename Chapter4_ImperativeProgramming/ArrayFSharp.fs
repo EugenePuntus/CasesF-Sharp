f5bb6f2afa6d6de08b1528ac4c033945b2c9edfc
PuntusEugene <kapral.20.it@gmail.com>
2018-01-31 17:26:29 +0300

    * end Chapter 3 start Chapter 4 - mutable values and array

---
+++ Chapter4_ImperativeProgramming/ArrayFSharp.fs
@@ -0,0 +1,49 @@
+﻿module ArrayFSharp
+open System
+
+let firstArray = [| for i in 1..10 -> i + 100 |]
+let secondArray = [| 1;2;4;5;6;7 |]
+
+// Алгоритм шифрования ROT13
+let letEncrypt (letter:char) =
+    if Char.IsLetter(letter) then
+        let newLetter = 
+            (int letter)
+            |> (fun letterIdx -> letterIdx - (int 'A'))
+            |> (fun letterIdx -> (letterIdx + 13)%26)
+            |> (fun letterIdx -> letterIdx + (int 'A'))
+            |> char
+        newLetter
+    else
+        letter
+
+let encryptText (text:char[]) = 
+    for idx = 0 to text.Length-1 do
+        text.[idx] <- letEncrypt text.[idx]
+
+let text = Array.ofSeq "ASDF GHJK QWER ZXC"
+
+printfn "Original: %s" <| new String(text)
+encryptText text
+printfn "Encrypt: %s" <| new String(text)
+encryptText text
+printfn "Decrypt: %s" <| new String(text)
+
+//срезы в массивах
+let daysOfWeek = Enum.GetNames( typeof<DayOfWeek> )
+daysOfWeek.[2..4] |> Array.iter (printf " %s ")
+printfn ""
+daysOfWeek.[4..] |> Array.iter (printf " %s ")
+printfn ""
+daysOfWeek.[..3] |> Array.iter (printf " %s ")
+printfn ""
+daysOfWeek.[*] |> Array.iter (printf " %s ")
+printfn ""
+
+
+let division = 3.0
+let twoPI = 2.0 * Math.PI;;
+
+let ar = Array.init (int division) (fun i -> float i * twoPI / division)
+
+ar |> Array.iter (printfn "%f")
---
+++ Chapter4_ImperativeProgramming/MutableValues.fs
@@ -0,0 +1,43 @@
+﻿module MutableValues
+open System
+
+//first test mutable
+let mutable message = "World"
+
+printfn "Hello, %s" message
+
+message <- "Zhenia"
+
+printfn "Hello, %s" message
+
+//using 'ref'
+let planets =
+    ref [
+        "Mercury"; "Venus"; "Earth";
+        "Mars"; "Jupiter"; "Saturn";
+        "Uranus"; "Neptune"; "Pluto"
+        ];;
+        
+!planets |> List.iter (printf "%s,");;
+
+planets := !planets |> List.filter (fun p -> p <> "Pluto");;
+printfn ""
+!planets |> List.iter (printf "%s,");;
+printfn ""
+
+// Mutable Record
+type MutableCar = { Make:string; Model:string; mutable Miles:int }
+
+let driveForASeason car = 
+    let rand = new Random()
+    car.Miles <- car.Miles + (rand.Next() % 10000)
+
+let myCar = { Make = "Italy"; Model = "Lancia"; Miles = 0 }
+
+driveForASeason myCar
+driveForASeason myCar
+driveForASeason myCar
+driveForASeason myCar
+
+printfn "%s %s %d" myCar.Make myCar.Model myCar.Miles
+
